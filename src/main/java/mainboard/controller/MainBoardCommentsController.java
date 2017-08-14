f6b2c5f1a88b596d60ea440c57f2289905b9acfd
KimNamhyuck <lasdkimn@naver.com>
2017-08-14 09:28:35 +0900

    controller

--- src/main/java/mainboard/controller/MainBoardCommentsController.java
+++ src/main/java/mainboard/controller/MainBoardCommentsController.java
@@ -5,20 +5,16 @@ import java.util.Map;
 
 import javax.inject.Inject;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import mainboard.model.CommentsDao;
 import mainboard.model.Comments;
+import mainboard.model.CommentsDao;
 import utility.Paging3;
 
 @Controller
